01e377527cef517f0ac3777d9c7596f1c69b47b9
Juan Lopez <juanquiroslopez@gmail.com>
2021-11-25 22:55:39 +0100

    algunos tests mas

--- src/main/java/samuraisword/samples/petclinic/card/CardRepository.java
+++ src/main/java/samuraisword/samples/petclinic/card/CardRepository.java
@@ -1,10 +1,16 @@
 package samuraisword.samples.petclinic.card;
 
 import java.util.Collection;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface CardRepository extends CrudRepository<Card, Integer> {
 
 	Collection<Card> findAll();
+	
+	@Query(value = "SELECT card FROM Cards WHERE name = :name" , nativeQuery = true)
+	Optional<Card> findByName(@Param("name") String name);
 }
