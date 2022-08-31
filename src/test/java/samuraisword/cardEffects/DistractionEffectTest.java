90292324edfd716c06d57acbab1a34487428f9d0
pedolirod <pedolirod@alum.us.es>
2022-08-31 20:31:36 +0200

    card effect tests

---
+++ src/test/java/samuraisword/cardEffects/DistractionEffectTest.java
@@ -0,0 +1,113 @@
+package samuraisword.cardEffects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import samuraisword.achievements.Achievement;
+import samuraisword.achievements.AchievementService;
+import samuraisword.achievements.AchievementType;
+import samuraisword.achievements.RolType;
+import samuraisword.character.CharacterService;
+import samuraisword.game.Game;
+import samuraisword.game.GamePhase;
+import samuraisword.game.GameService;
+import samuraisword.character.Character;
+import samuraisword.player.Player;
+import samuraisword.player.PlayerService;
+import samuraisword.player.Rol;
+import samuraisword.samples.petclinic.card.Card;
+import samuraisword.samples.petclinic.card.CardService;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+public class DistractionEffectTest {
+
+	@Autowired
+	protected CharacterService characterService;
+	@Autowired
+	protected CardService cardService;
+	@Autowired
+	protected GameService gameService;
+	@Autowired
+	protected PlayerService playerService;
+
+	@Test
+	void distractionWorksWell() {
+		Game g = new Game();
+		createTestGame(g);
+		Player p = g.getListPlayers().get(0);
+		
+		List<Card>lh = p.getHand();
+		int i = 0 + (int)(Math.random() * ((lh.size() - 0)));
+		cardService.discard(lh.get(i).getName(), p.getHand(), g.getDiscardPile());
+		
+	}
+
+	private void createTestGame(Game game) {
+		game.setDeck(cardService.createDeck());
+		game.setDiscardPile(new ArrayList<>());
+
+		List<Player> players = game.getListPlayers();
+
+		// players de prueba
+		Player p1 = playerService.findById(1).get();
+
+		Player p2 = playerService.findById(2).get();
+
+		Player p3 = playerService.findById(3).get();
+
+		Player p4 = playerService.findById(4).get();
+
+		Player p5 = playerService.findById(5).get();
+
+		Player p6 = playerService.findById(6).get();
+
+		Player p7 = playerService.findById(7).get();
+
+		players.add(p1);
+		players.add(p2);
+		players.add(p3);
+		players.add(p4);
+		players.add(p5);
+		players.add(p6);
+		players.add(p7);
+
+		gameService.asignCharacterAndHearts(players);
+		gameService.asignRolAndHonor(players);
+		gameService.asignOrder(players);
+
+		game.setListPlayers(players);
+		game.setCurrentPlayer(players.get(0));
+		game.getCurrentPlayer().setWeaponBonus(1);
+	
+
+		for (Player player : game.getListPlayers()) {
+			player.setGame(game);
+			player.setEquipment(new ArrayList<>());
+			player.setNArmor(0);player.setNFastDraw(0);player.setNFocus(0);
+			playerService.savePlayer(player);
+		}
+
+		gameService.asignCards(game.getDeck(), players);
+		gameService.processDrawPhase(game);
+		game.setGamePhase(GamePhase.MAIN);
+
+		gameService.asignCards(game.getDeck(), players);
+
+	}
+
+}
